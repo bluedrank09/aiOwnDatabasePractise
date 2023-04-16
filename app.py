8081675f6a0c0183eafa9053cf9a539020b49eba
mayababu <mayababu73@gmail.com>
2023-04-16 18:35:19 +1200

    have interface working, need to get more info on actors

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os # most likely to be used to get api key ?
 from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, GPTListIndex, LLMPredictor, PromptHelper
 from langchain.chat_models import ChatOpenAI
 
-os.environ["OPENAI_API_KEY"] = ""
+openai_api_key = os.environ.get('OPENAI_API_KEY')
 
 def construct_index(directory_path): # contructing nodes and grouping them to use later for KNN. we need drectry pat to get to te pdfs
     max_input_size = 4096 # bytes ? pixels ? characters? check
@@ -13,17 +13,20 @@ def construct_index(directory_path): # contructing nodes and grouping them to us
     max_chunk_overlap = 20
     chunk_size_limit = 600
 
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit = chunk_size_limit)
-    llm_predictor = LLMPredictor(llm = ChatOpenAI(temperature = 0.7, model_name = 'gpt-3.5-turbo', max_tokens = num_outputs))
-    documents = SimpleDirectoryReader(directory_path).load_data
-    index = GPTSimpleVectorIndex(documents, llm_predictor = llm_predictor, prompt_helper = prompt_helper)
+    # prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit = chunk_size_limit)
+    # llm_predictor = LLMPredictor(llm = ChatOpenAI(temperature = 0.7, model_name = "gpt-3.5-turbo", max_tokens = num_outputs))
+    # documents = SimpleDirectoryReader(directory_path).load_data
+    # index = GPTSimpleVectorIndex(documents, llm_predictor = llm_predictor, prompt_helper = prompt_helper)	
+
+    documents = SimpleDirectoryReader(directory_path).load_data()
+    index = GPTSimpleVectorIndex.from_documents(documents)
 
     index.save_to_disk("index.json") 
 
     return(index)
 
 def chatBot(input_text):
-    index = GPTSimpleVectorIndex.load_from_disk("index.json")
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
     response = index.query(input_text, response_mode = 'compact')
     
     return(response.response)
